27e77f987e28ba7825276929e0fc353d4371fcae
lwzm <lwzm@qq.com>
2021-12-31 10:28:28 +0800

    feat: auto append configurations

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Docker privoxy
 
 ```
-docker run -p 8118:8118 lwzm/privoxy
+docker run -p 8118:8118 lwzm/privoxy [option-line] [option-line-2] ...
+```
+
+e.g.
+
+```
+docker run -p 8118:8118 lwzm/privoxy "forward-socks5 / xxx-socks5-server:1080 ."
 ```
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,16 @@
 
 cat >config <<'EOF'
 listen-address 0.0.0.0:8118
+socket-timeout 300
 keep-alive-timeout 300
 default-server-timeout 60
 connection-sharing 1
 EOF
 
-"$@"
+for cfg in "$@"; do
+	echo "$cfg" >>config
+done
+
+cat config
 
 exec privoxy --no-daemon
